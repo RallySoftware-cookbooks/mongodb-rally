f665e75069071adb3b326bb7ef585e03583b5433
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-07-25 09:08:37 -0600

    Removing unused chefspec test

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -8,12 +8,6 @@ describe '::default' do
     expect(chef_run).to install_package 'mongo-10gen'
   end
 
-  it 'should install version of mongo-10gen specified' do
-    chef_run.node.set['mongodb-rally']['version'] = 'foo'
-    chef_run.converge 'mongodb-rally::default'
-    expect(chef_run).to install_package 'mongo-10gen-foo'
-  end
-
   it 'should include 10gen yum repository for mongo-10gen if the platform family is rhel' do
     chef_run.node.automatic_attrs['platform_family'] = 'rhel'
     chef_run.converge 'mongodb-rally::default'

fd0fa59a33ec87f35cfcb03a20729b0aa81b8214
Russ Teabeault and Matt Farrar <rteabeault@rallydev.com>
2013-10-10 11:11:29 -0600

    Updating to use VERSION file.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rallysoftware-cookbooks@rallydev.com'
 license          'MIT'
 description      'Installs/Configures mongodb-rally'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 recipe 'mongodb-rally::default', 'Includes the client recipe by default'
 recipe 'mongodb-rally::client', 'Installs mongo shell & client packages and tools'
